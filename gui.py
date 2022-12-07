e7448b47682e168330946dfa2f753b30b0d9c825
JosefNadler <Josef.Nadler@outlook.com>
2022-12-07 10:40:36 +0100

    Implement the edit button

--- gui.py
+++ gui.py
@@ -4,22 +4,41 @@ import PySimpleGUI as sg
 label = sg.Text("Type in a todo")
 input_box = sg.InputText(key='-todo-', tooltip='Enter todo')
 add_button = sg.Button("Add")
+list_box = sg.Listbox(values=functions.get_todos(), key='-todos-', enable_events=True, size=[45, 10])
+edit_button = sg.Button("Edit")
 
-win = sg.Window('My todo App', layout=[[label], [input_box, add_button]], font=('Helvetica', 20))
+win = sg.Window('My todo App', layout=[[label], [input_box, add_button], [list_box, edit_button]], font=('Helvetica', 20))
 
 while True:
     event, values = win.read()
     print(event, values)
 
+
     if event in (sg.WIN_CLOSED, 'Exit'):
+        print('First Exit')
         break
     else:
+        print(f"The event is {event} and -todo- value is {values['-todo-']}, and the -todos- value is {values['-todos-']}")
         match event:
             case 'Add':
                 todos = functions.get_todos()
                 new_todo = values['-todo-'] + '\n'
                 todos.append(new_todo)
                 functions.save_todos(todos)
+                win['-todos-'].update(values=todos)
+            case 'Edit':
+                todo_to_edit = values['-todos-'][0]
+                new_todo = values['-todo-']
 
+                todos = functions.get_todos()
+                index = todos.index(todo_to_edit)
+                todos[index] = new_todo + '\n'
+                functions.save_todos(todos)
+                win['-todos-'].update(values=todos)
+            case '-todos-':
+                win['-todo-'].update(value=values['-todos-'][0])
+            case sg.WIN_CLOSED:
+                print('Second Exit')
+                break
 
 win.close()
